77ad7cc5d1ed74bb2b315bebed1931d8c49e0f20
Ondrej  Pekacek <ondrej.pekacek@fsv.cuni.cz>
2022-07-14 17:42:29 +0200

    feat: add named entity recognition workflow and scripts

---
+++ 4. data_analysis/named_entity_recognition/ner_rstudio_jobs.R
@@ -0,0 +1,23 @@
+source(file.path("ner_nametag_api.R"))
+
+for (ner_file in all_chunks_path_ner) {
+
+  one_chunk <- readRDS(ner_file)
+
+  nametag_df_chunk <- nametag_process(article_id = one_chunk$article_id,
+                                      article_text = one_chunk$text,
+                                      log = FALSE)
+
+  saveRDS(nametag_df_chunk, paste0(file.path("data", "nametag_") , basename(ner_file)))
+
+  rm(nametag_df_chunk, one_chunk)
+  gc()
+
+  cat("\nThe following chunk was processed by NameTag and saved locally:", basename(ner_file), "\n")
+
+  # Pause cca 10 mins between chunks
+  Sys.sleep(abs(rnorm(1, 600, sd = 100)))
+
+}
+
+
